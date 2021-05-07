f52dc89342c0789aebf6ba76092aa45cba46aa63
t.babparn <tp.babparn@gmail.com>
2021-05-07 20:48:14 +0700

    first commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,114 @@
+import Img1 from '../../images/svg-1.svg';
+import Img2 from '../../images/svg-2.svg';
+import Img3 from '../../images/svg-3.svg';
+import Img4 from '../../images/svg-4.svg';
+import Img5 from '../../images/svg-5.svg';
+import Img6 from '../../images/svg-6.svg';
+
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: Img1,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Learn more',
+  imgStart: false,
+  img: Img2,
+  alt: 'Credit',
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our team',
+  headline: 'Create an account extremely easy',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Start now',
+  imgStart: true,
+  img: Img3,
+  alt: 'Swipe',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjFour = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: Img4,
+  alt: 'Document',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjFive = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Learn more',
+  imgStart: false,
+  img: Img5,
+  alt: 'Message',
+  dark: false,
+  primary: true,
+  darkText: true,
+};
+
+export const homeObjSix = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our team',
+  headline: 'Create an account extremely easy',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+  buttonLabel: 'Start now',
+  imgStart: true,
+  img: Img6,
+  alt: 'Gallery',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
